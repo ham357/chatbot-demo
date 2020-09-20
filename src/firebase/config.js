8d7037dd8010c625b2f218c3f0f435e3d1fe46d6
ham357 <sakuraniumarete@gmail.com>
2020-09-21 02:01:48 +0900

    firebaseでAPIを作成

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBo5cx63KD5ihPZtZZBcMnkmdRYXU_1ygQ",
+    authDomain: "chatbot-demo-8d8d5.firebaseapp.com",
+    databaseURL: "https://chatbot-demo-8d8d5.firebaseio.com",
+    projectId: "chatbot-demo-8d8d5",
+    storageBucket: "chatbot-demo-8d8d5.appspot.com",
+    messagingSenderId: "493136741666",
+    appId: "1:493136741666:web:74c0d18f9f51209831917b",
+    measurementId: "G-2WYMNS75J4"
+  };
+
+export default firebaseConfig
